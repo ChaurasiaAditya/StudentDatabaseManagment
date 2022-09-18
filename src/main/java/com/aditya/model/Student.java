b51be6fd6c3572ddf890126f33784dcc6864323c
Aditya Chaurasia <chaurasiaaditya755@gmail.com>
2022-09-18 23:51:56 +0530

    added the Student class

--- src/main/java/com/aditya/model/Student.java
+++ src/main/java/com/aditya/model/Student.java
@@ -4,7 +4,51 @@
  * Created With: IntelliJ IDEA Community Edition
  * Profile: github.com/ChaurasiaAditya
  */
+
 package com.aditya.model;
+import java.util.Objects;
 
 public class Student {
+    private String name;
+    private int rollNumber;
+    private int grade;
+
+    public Student(String name, int rollNumber, int grade) {
+        this.name = name;
+        this.rollNumber = rollNumber;
+        this.grade = grade;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getRollNumber() {
+        return rollNumber;
+    }
+
+    public int getGrade() {
+        return grade;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Student student = (Student) o;
+        return rollNumber == student.rollNumber && grade == student.grade && Objects.equals(name, student.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, rollNumber, grade);
+    }
+
+    @Override
+    public String toString() {
+        return " Name = " + getName() +
+                ", RollNumber = " + getRollNumber() +
+                ", Grade = " + getGrade() +
+                " \n";
+    }
 }
