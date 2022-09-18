6a5cd23417c51eef32f974a669a07a1cdde170d8
Aditya Chaurasia <chaurasiaaditya755@gmail.com>
2022-09-18 23:49:19 +0530

    added student class

---
+++ src/main/java/com/aditya/model/Student.java
@@ -0,0 +1,10 @@
+/*
+ * Author Name: Aditya Chaurasia
+ * Date: 18-09-2022
+ * Created With: IntelliJ IDEA Community Edition
+ * Profile: github.com/ChaurasiaAditya
+ */
+package com.aditya.model;
+
+public class Student {
+}
